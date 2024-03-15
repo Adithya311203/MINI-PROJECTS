5316e074f1fcb0714c77776fb8f2ae673b98ee27
Adithya311203 <97468387+Adithya311203@users.noreply.github.com>
2024-03-15 23:36:27 +0530

    website

---
+++ Website/script.js
@@ -0,0 +1,3 @@
+const APILINK='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9f637d696252e74b2d094283bc9d2438&page1';
+const IMG_PATH='https://image.tmdb.org/t/p/w1280';
+const SEARCH_API="https://api.themoviedb.org/3/search/movie?&api_key=9f637d696252e74b2d094283bc9d2438&query=";
