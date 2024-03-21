b4a04d036d5db75d9b8049f9f89b5020412ea45e
Adithya311203 <97468387+Adithya311203@users.noreply.github.com>
2024-03-21 23:03:53 +0530

    Update index.js

--- Website/index.js
+++ Website/index.js
@@ -1,1 +1,3 @@
-const APILINK='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9f637d696252e74b2d094283bc9d2438&page1';
+import app from "./server.js"
+import mongodb from "mongodb"
+import ReviewsDAO from "./dao/reviewsDAO.js"
